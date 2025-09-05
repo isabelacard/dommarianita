8f8a55e73eafe7ac06fd36f694a02bce3234fbdd
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-09-05 05:07:52 -0500

    3lista

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,18 +1,18 @@
 
-// Paso 1: Selecciona el botón con document.getElementById
+// Paso 1: Selecciona el input y la lista
 
-const boton = document.getElementById ('btnColor')
-let Pink = false
+const input = document.getElementById('entrada')
+const lista = document.getElementById('lista')
 
-// Paso 2: Agrega un event listener al botón para detectar el click
+// Paso 2: Agrega un listener al input para detectar Enter (keyup o keydown)
 
-boton.addEventListener('click', () => {
-  if (Pink) {
-    document.body.style.backgroundColor = "pink"
-  } else {
-    document.body.style.backgroundColor = 'white'
+input.addEventListener ('keyup', (Enter) => {
+  if (Enter.key === 'Enter') {
+    // Paso 3: Crea un nuevo <li> con el valor del input y agrégalo al ul
+    const li = document.createElement('li')
+    li.textContent = entrada.value;
+    lista.appendChild(li)
+    // Paso 4: Limpia el input después de agregar
+    entrada.value = '';
   }
-  Pink = !Pink
 })
-
-// Paso 3: Cambia el color de fondo de body dinámicamente
